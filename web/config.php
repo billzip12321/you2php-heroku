3b82b1d9f66e305857a4aa8a48db8885174de340
billzip12321 <busifans@gmail.com>
2018-05-10 21:24:27 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,7 +1,7 @@
 <?php
 define('ROOT_PART', Root_part());
 define('APIKEY', getenv("AIzaSyDODIAoHA02ubi6MEl20MNjgqoFs0q7Nc8"));
-define('GJ_CODE', getenv("TW"));
+define('GJ_CODE', getenv("US"));
 define('SITE_NAME',getenv("REDTUBE"));
 define('TITLENAME', getenv("REDTUBE"));
 define('EN2DEKEY',getenv("EN2DEKEY"));
