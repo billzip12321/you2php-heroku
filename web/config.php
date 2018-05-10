64cc4c79f2f09dc403749463909c468863f8def9
billzip12321 <busifans@gmail.com>
2018-05-10 21:30:56 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', getenv("APIKEY"));
-define('GJ_CODE', getenv("GJ_CODE"));
-define('SITE_NAME',getenv("SITE_NAME"));
-define('TITLENAME', getenv("TITLENAME"));
+define('APIKEY', getenv("AIzaSyDODIAoHA02ubi6MEl20MNjgqoFs0q7Nc8"));
+define('GJ_CODE', getenv("US"));
+define('SITE_NAME',getenv("REDTUBE"));
+define('TITLENAME', getenv("REDTUBE"));
 define('EN2DEKEY',getenv("EN2DEKEY"));
-define('EMAIL', getenv("EMAIL"));
+define('EMAIL', getenv("GWU1@MADISONCOLLEGE.EDU"));
 ?>
